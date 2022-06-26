b650275fe3623f557dfa77dc18892ce74705e06f
Sarah Thompson <sthompson@fintechprocessing.com>
2022-06-26 13:09:40 -0400

    created horse and user controllers

---
+++ server/controllers/horse.js
@@ -0,0 +1,71 @@
+const Horse = require("../models/horses");
+const User = require("../models/users");
+
+const horseControllers = {
+    //get all horses 
+    async totalHorses(req, res) {
+      try {
+        const horses = await Horse.find({});
+        if (!horses.length) {
+          res.json({ message: "No horses found." });
+          return;
+        }
+        res.json(horses);
+      } catch (error) {
+        res.json(error);
+      }
+    },
+
+    //new horse
+    async newHorse({ body }, res) {
+      console.log("body =", body);
+       try {
+         const newHorse = await Horse.create(body);
+         console.log("newHorse = ", newHorse)
+         if (!newHorse) {
+           res.status(503).json({ message: "Horse not created." });
+         }
+         res.json(newHorse);
+       } catch (error) {
+         console.log(error);
+         res.json(error);
+       }
+     },
+
+    //get specific horse by id - this can be used to edit I believe
+    async getHorseById({ params }, res) {
+      try {
+        const horseData = await Horse.findOne({ _id: params.id })
+          .populate('thoughts') //need to edit this
+          .populate('friends')
+          .select(["-_v", "-_id", "-email"]);
+        if (!horseData) {
+          res.json({ message: "Horse not found." });
+          return;
+        }
+        res.json(horseData);
+      } catch (error) {
+        res.json(error);
+      }
+    },
+
+    //delete a horse 
+    async deleteHorse({ params }, res) {
+      try {
+        const horseData = await Horse.findOneAndDelete(
+          { _id: params.id },
+          { new: true }
+        );
+
+        if (!horseData) {
+          res.json({ message: "Horse not found." })
+        }
+
+        res.json(horseData);
+      } catch (error) {
+        res.json(error);
+      }
+    },
+  };
+
+  module.exports = horseControllers;
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,76 @@
 const horses = require("../models/horses");
 const User = require("../models/users");
 
+const userControllers = {
+    //get all users 
+    async totalUsers(req, res) {
+      try {
+        const users = await User.find({});
+        if (!users.length) {
+          res.json({ message: "No users found." });
+          return;
+        }
+        res.json(users);
+      } catch (error) {
+        res.json(error);
+      }
+    },
+
+    //new user
+    async newUser({ body }, res) {
+      console.log("body =", body);
+       try {
+         const newUser = await User.create(body);
+         console.log("newUser = ", newUser)
+         if (!newUser) {
+           res.status(503).json({ message: "User not created." });
+         }
+         res.json(newUser);
+       } catch (error) {
+         console.log(error);
+         res.json(error);
+       }
+     },
+
+    //get specific user by id - this could be used for getting a specific horse
+    async getUserById({ params }, res) {
+      try {
+        const userData = await User.findOne({ _id: params.id })
+          .populate('thoughts')
+          .populate('friends')
+          .select(["-_v", "-_id", "-email"]);
+        if (!userData) {
+          res.json({ message: "User not found." });
+          return;
+        }
+        res.json(userData);
+      } catch (error) {
+        res.json(error);
+      }
+    },
+
+    //delete a user 
+    async deleteUser({ params }, res) {
+      try {
+        const userData = await User.findOneAndDelete(
+          { _id: params.id },
+          { new: true }
+        );
+
+        if (!userData) {
+          res.json({ message: "User not found." })
+        }
+
+        res.json(userData);
+      } catch (error) {
+        res.json(error);
+      }
+    },
+  };
+
+  module.exports = userControllers;
+
 //social network api controllers and routes 
 
-create get and post for user and for horses
-find a template i like 
+//create get and post for user and for horses
+//find a template i like 
